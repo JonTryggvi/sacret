8901419d6c521faa7cb4d17b6518806a3876d1f0
JonTryggvi <jontryggvi@jontryggvi.is>
2022-05-16 10:24:09 +0200

    see if it deploys

---
+++ library/src/js/components/component-card 3.js
@@ -0,0 +1,24 @@
+const CardHoverState = {
+  init(scope){
+    this.cacheDom(scope)
+    this.addEvents()
+  },
+  cacheDom(scope){
+    this.body = scope
+    this.uniCards = this.body.querySelectorAll('.uni_card')
+  },
+  addEvents(){
+    this.uniCards.forEach(card => {
+      card.onmouseenter = (e) => {
+          e.target.closest('.uni_card').classList.add('hovering')
+      }
+      card.onmouseleave = (e) => {
+        setTimeout(() => {
+          e.target.closest('.uni_card').classList.remove('hovering')
+        }, 1000);
+      }
+    })
+
+  }
+}
+module.exports ={ CardHoverState }
