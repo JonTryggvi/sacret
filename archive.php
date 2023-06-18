a726207ee739e9c6a1600782b41d2b6bbd3f86e6
Jón Tryggvi Unnarsson <jontryggvi@jontryggvi.is>
2023-06-18 18:48:39 +0200

    fix archive animaaion card contarin ID

--- archive.php
+++ archive.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 	if (!defined('ABSPATH')) exit;
 	get_header();
 ?>
 	<main id="mainarea" class="mainarea" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-							
+
 	<?php if (have_posts()) : ?>
 		<section class="uni_section uni_section__has_title uni_section__posts  archive grid-with-margin" data-post-type="post" data-term-id="<?php echo get_queried_object()->term_id; ?>">
 			<?php the_archive_title( '<h2>', '</h2>' ); ?>
-			<div class="card-container"></div> 
+			<div id="cardContainerArchive" class="card-container"></div>
 			<?php 	uni_partial('parts/components/loadmore-button');	?>
 		</section>
 	<?php else : ?>
